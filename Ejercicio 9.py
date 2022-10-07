2205c0fae8ef99285e2c6fc581146e8f570e92fc
Leyre <lgarciarob@eduacacion.navarra.es>
2022-10-07 16:52:44 +0200

    Ejercicios añadidos

--- Ejercicio 9.py
+++ Ejercicio 9.py
@@ -1,3 +1,11 @@
 #Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual y el número de años, y muestre
 # por pantalla el capital obtenido en la inversión.
 
+inversion = float(input("Cuanto dinero quiere invertir?"))
+interesAnual = float(input("Cuanto %?"))
+años = int(input("Cuantos años?"))
+
+capitalObtenido = inversion * (interesAnual / 100) * años + (inversion)
+
+print ("El capital obtenido en la inversion es de " + str(inversion) + " es " + str(capitalObtenido))
+
